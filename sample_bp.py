aa90ba49cfa433cbe0fb6bf7dfd5fd87edda18a9
Charlie <trollol678534@gmail.com>
2021-03-24 18:41:31 -0700

    experment

--- sample_bp.py
+++ sample_bp.py
@@ -1,7 +1,11 @@
 from flask import Blueprint
-
+from flask import render_template, request, redirect, url_for, session, flash, Flask, Response, Blueprint
 bp = Blueprint('example_blueprint', __name__)
 
 @bp.route('/')
 def index():
     return "Hello World!"
+
+@bp.route2('/')
+def purpose():
+    return render_template("base.html")

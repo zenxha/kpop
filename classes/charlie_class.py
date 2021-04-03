5c7de064374e13f1e4af239ce0a11bcf2a3e32b1
Charlie <trollol678534@gmail.com>
2021-04-02 23:02:21 -0700

    minilab

--- classes/charlie_class.py
+++ classes/charlie_class.py
@@ -1,34 +1,18 @@
+import datetime
 
-
-class games:
-    def __init__(self, games):
-        if len(games) < 0:
-            raise ValueError("Songs must be greater than 0")
-        self._games = games
-        if not games:
-            self._games = [{
-                "title": "ARK: Survival Evolved",
-                "releasedate": "June 2, 2015",
-                "randomfact": "The game was designed by Kayd Hendricks.",
-
-            },
-                {
-                    "title": "Risk of Rain 2",
-                    "releasedate": "March 28, 2019",
-                    "randomfact": "It's a roguelike third-person shooter developed by Hopoo Games",
-                },
-                {
-                    "title": "The Binding of Isaac",
-                    "releasedate": "September 28, 2011",
-                    "randomfact": "The game was the result of a week-long game jam between McMillen and Himsl to develop a The Legend of Zelda-inspired roguelike",
-                },
-                {
-                    "title": "Minecraft",
-                    "releasedate": "November 18, 2011",
-                    "randomfact": "The game was created by Markus Notch",
-                }]
+class game:
+    def __init__(self, title,releasedate,randomfact=None):
+        self._title = title
+        self._releasedate = releasedate
+        self._randomfact = randomfact
 
     def calculateage(self):
-
-    def getgames(self):
-        return self._games
+        release_obj = datetime.datetime.strptime(self._releasedate, '%Y-%m-%d')
+        diff = datetime.datetime.now() - release_obj
+        return diff
+    def gettitle(self):
+        return self._title
+    def getreleasedate(self):
+        return self._releasedate
+    def getrandomfact(self):
+        return self._randomfact

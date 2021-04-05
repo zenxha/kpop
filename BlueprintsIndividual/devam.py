f8eb7476dfdf8926edd16642913b323290a47db3
Chris-Ru <72892271+Chris-Ru@users.noreply.github.com>
2021-04-05 12:38:15 -0700

    fix some of devam's code pt.2

--- BlueprintsIndividual/devam.py
+++ BlueprintsIndividual/devam.py
@@ -2,15 +2,20 @@ from flask import Blueprint, render_template, jsonify, request
 import json
 from classes.class_test import Video
 
-ds = Blueprint('ds', __name__ ,url_prefix='/devam', static_folder="static", template_folder="templates")
+ds = Blueprint('ds', __name__, url_prefix='/devam', static_folder="static", template_folder="templates")
+
 
 @ds.route('/')
 def index():
     video = Video('Music Videos', 'idk').get_similar()
     return video
+
+
 @ds.route('/musicvids', methods=["POST", 'GET'])
 def musicvids():
-    if(request.method == 'POST'):
+    if (request.method == 'POST'):
         creator = request.form.get('creator')
         vidname = request.form.get('vidname')
-        return render_template('getMV.html', video = Video(creator, vidname))
+        return render_template('getMV.html', video=Video(creator, vidname))
+    return render_template('getsongs.html', background="https://cdn.wallpapersafari.com/91/31/z4AvR6.jpg",
+                           song=Song('Official+HIGE+DANdism', "pretender"))
--- classes/class_test.py
+++ classes/class_test.py
@@ -1,9 +1,14 @@
-
 import requests
 import json
+import os
+
+#import google_auth_oauthlib.flow
+#import googleapiclient.discovery
+#import googleapiclient.errors
+
 # http://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=official+hige+dandism&track=pretender&api_key=630846faaf3ca8d5cf4d712e56bd4989&format=json
 class Song:
-    """Initializer of class takes series parameter and returns Class Object"""
+    """Initializer of class takes series parameter and returns Class Objectg"""
     def __init__(self, artist, song):
         self._artist = artist
         self._song = song
@@ -14,3 +19,32 @@ class Song:
 
         return response.json()
 
+class Video:
+    def __init__(self, creator, vidname):
+        self._creator = creator
+        self._vidname = vidname
+    scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
+
+    def main():
+        # Disable OAuthlib's HTTPS verification when running locally.
+        # *DO NOT* leave this option enabled in production.
+        os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+        api_service_name = "youtube"
+        api_version = "v3"
+        client_secrets_file = "YOUR_CLIENT_SECRET_FILE.json"
+
+        # Get credentials and create an API client
+        flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+            client_secrets_file, scopes)
+        credentials = flow.run_console()
+        youtube = googleapiclient.discovery.build(
+            api_service_name, api_version, credentials=credentials)
+
+        request = youtube.channels().list(
+            part="contentDetails",
+            forUsername="AOMGOFFICIAL"
+        )
+        response = request.execute()
+
+        return(response)

867c513eecbd0d694098b08756bf704c64dd46c8
Chris-Ru <72892271+Chris-Ru@users.noreply.github.com>
2021-04-22 18:17:07 -0700

    bubble sort, add some finishing touches

--- BlueprintsIndividual/chris.py
+++ BlueprintsIndividual/chris.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template
-from classes.chris_class import song
+from flask import Blueprint, render_template, request
+from classes.chris_class import song, BubbleSort
 
 
 cr = Blueprint('cr', __name__, url_prefix="/cr", static_folder="static", template_folder="templates")
@@ -10,21 +10,42 @@ def index():
     songs = [{
             "title": "Party Rock Anthem",
             "link": "https://www.youtube.com/embed/KQ6zr6kCPj8",
-            "description":"LITERALLY EVERYONE LOVES THIS SONG"
-        },
-        {
+            },
+            {
             "title": "Uptown Funk",
             "link": "https://www.youtube.com/embed/OPf0YbXqDm0",
-            "description":"Some people like this song"
-        },
-        {
+            },
+            {
             "title": "Wrecking Ball",
             "link": "https://www.youtube.com/embed/My2FRPA3Gf8",
-            "description":"nobody likes this song"
-        },
-        {
+            },
+            {
             "title": "A Thousand Miles",
             "link": "https://www.youtube.com/embed/Cwkej79U3ek",
-            "description":"This song is just really catchy"
-        }]
+            }]
     return render_template('shuffle.html', song=song(songs), songarray=songs)
+
+@cr.route('/bubble', methods = ["GET","POST"])
+def bubbleSort():
+    arr = []
+    isString = False
+    if request.form:
+        string = request.form.get("string")
+        arr = string.split()
+        original = string.split()
+        if(request.form["select"] == "integer"):
+            try:
+                for j in range (0,len(arr)):
+                    arr[j] = int(arr[j])
+                for j in range (0,len(original)):
+                    original[j] = int(original[j])
+                return render_template("crbubblesort.html",output_list = BubbleSort(arr,isString).OuputList,original_list = original)
+            except ValueError:
+                return render_template("crbubblesort.html",output_list = "Can Only Contain String or Integer",original_list = "Error")
+        else:
+            try:
+                isString = True
+                return render_template("crbubblesort.html",output_list = BubbleSort(arr,isString).OuputList,original_list = original)
+            except ValueError:
+                return render_template("crbubblesort.html",output_list = "Can Only Contain String or Integer",original_list = "Error")
+    return render_template("crbubblesort.html",output_list = BubbleSort(arr,isString).OuputList,original_list = arr)
--- classes/chris_class.py
+++ classes/chris_class.py
@@ -2,6 +2,7 @@ import random
 
 class song:
     def __init__(self, songs):
+        self._list = []
         if len(songs) < 0:
             raise ValueError("Songs must be greater than 0")
         self._songs = songs
@@ -23,6 +24,83 @@ class song:
                                 "link": "https://www.youtube.com/embed/Cwkej79U3ek",
                             }]
 
+    @property
+    def list(self):
+        return self._songs
+
+
     def songshuffle(self):
         random.shuffle(self._songs)
         return self._songs
+
+
+
+
+class BubbleSort:
+    def __init__(self, input_list,isString):
+
+        # init/declare the variables
+        self.input_list = input_list
+
+
+        # init the outpu
+        self._output_list = []
+
+        # init the functions
+        self.bubbleSort(input_list)
+        # self.OuputList()
+        self.isString = isString
+
+    def bubbleSort(self, arr):
+        n = len(arr)
+
+        # Traverse through all array elements
+        for i in range(n):
+
+            # Last i elements are already in place
+            for j in range(0, n-i-1):
+
+                # traverse the array from 0 to n-i-1
+                # Swap if the element found is greater
+                # than the next element
+                if arr[j] > arr[j+1] :
+                    arr[j], arr[j+1] = arr[j+1], arr[j]
+    def stringSort(self, arr):
+        alpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'j', 'K', 'L',
+                 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W','X',
+                 'Y','Z']
+        for j in range(0, len(arr)):
+            for i in range(0, len(arr)):
+                _sorted = False
+                if i != len(arr)-1:
+                    for k in range(0,len(arr[i])):
+                        if not _sorted:
+                            if k != (len(arr[i]) and len(arr[i+1])):
+                                if alpha.index(arr[i][k]) > alpha.index(arr[i+1][k]):
+                                    arr[i], arr[i+1] = arr[i+1], arr[i]
+                                    _sorted = True
+                                elif alpha.index(arr[i][k]) < alpha.index(arr[i+1][k]):
+                                    _sorted = True
+                                else:
+                                    if len(arr[i+1]) < len(arr[i]):
+                                        arr[i], arr[i+1] = arr[i+1], arr[i]
+                                        _sorted = True
+    def Stringconvert(self,arr):
+        for j in range(0, len(arr)):
+            arr[j] = arr[j].upper()
+
+
+    @property
+    def OuputList(self):
+        arr = self.input_list
+        if(self.isString):
+            self.Stringconvert(arr)
+            self.stringSort(arr)
+            for i in range(len(arr)):
+                self._output_list.append(arr[i])
+            return self._output_list
+        else:
+            self.bubbleSort(arr)
+            for i in range(len(arr)):
+                self._output_list.append(arr[i])
+            return self._output_list

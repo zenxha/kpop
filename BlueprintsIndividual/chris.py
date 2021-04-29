316f3945f4532684ec558073a5a4ec52c24af9d9
Chris-Ru <72892271+Chris-Ru@users.noreply.github.com>
2021-04-28 22:31:45 -0700

    fix variable names in chris.py bubblesort

--- BlueprintsIndividual/chris.py
+++ BlueprintsIndividual/chris.py
@@ -12,26 +12,26 @@ def shuffle():
 
 @cr.route('/bubble', methods = ["GET","POST"])
 def bubbleSort():
-    arr = []
+    inarr = []
     isString = False
     if request.form:
         string = request.form.get("string")
-        arr = string.split()
+        inarr = string.split()
         input = string.split()
         if(request.form["select"] == "string"):
             try:
                 isString = True
-                return render_template("crbubblesort.html",sorted_list = BubbleSort(arr,isString).OutputList,input_list = input)
+                return render_template("crbubblesort.html",sorted_list = BubbleSort(inarr,isString).OutputList,input_list = input)
             except ValueError:
                 return render_template("crbubblesort.html",sorted_list = "Can Only Contain A String",input_list = "Error")
         else:
             try:
-                for j in range (0,len(arr)):
-                    arr[j] = int(arr[j])
+                for j in range (0,len(inarr)):
+                    inarr[j] = int(inarr[j])
                 for j in range (0,len(input)):
                     input[j] = int(input[j])
-                return render_template("crbubblesort.html",sorted_list = BubbleSort(arr,isString).OutputList,input_list = input)
+                return render_template("crbubblesort.html",sorted_list = BubbleSort(inarr,isString).OutputList,input_list = input)
             except ValueError:
                 return render_template("crbubblesort.html",sorted_list = "Can Only Contain An Integer",input_list = "Error")
 
-    return render_template("crbubblesort.html",sorted_list = BubbleSort(arr,isString).OutputList,input_list = arr)
+    return render_template("crbubblesort.html",sorted_list = BubbleSort(inarr,isString).OutputList,input_list = inarr)

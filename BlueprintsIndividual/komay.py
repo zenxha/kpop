002f17d57dea9f470ef49f3225ee6204c7183b3b
zenxha <komays5042@gmail.com>
2021-05-01 06:53:28 -0700

    add komay to navbar

--- BlueprintsIndividual/komay.py
+++ BlueprintsIndividual/komay.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, jsonify, request
 import json
 from classes.komay_class import Song
 
-ks = Blueprint('ks', __name__ ,url_prefix='/komay', static_folder="static", template_folder="templates")
+ks = Blueprint('ks', __name__ ,url_prefix='/ks', static_folder="static", template_folder="templates")
 
 @ks.route('/')
 def index():
@@ -14,6 +14,7 @@ def index():
         return render_template('getsongs.html', song = Song(artist, song, 'similarity'), background="https://cdn.wallpapersafari.com/91/31/z4AvR6.jpg")
 
     return render_template('getsongs.html', background="https://cdn.wallpapersafari.com/91/31/z4AvR6.jpg", song = Song('Queen', "Bohemian+Rhapsody", "similarity"))
+@ks.route('/')
 def bubbleSort():
     if(request.method == 'POST'):
         artist = request.form.get('artist')

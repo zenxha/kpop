00b33c6474ba529ec738bd00b7ef394a824c08ff
zenxha <komays5042@gmail.com>
2021-05-01 06:35:43 -0700

    fix

--- BlueprintsIndividual/komay.py
+++ BlueprintsIndividual/komay.py
@@ -6,10 +6,15 @@ ks = Blueprint('ks', __name__ ,url_prefix='/komay', static_folder="static", temp
 
 @ks.route('/')
 def index():
-    song = Song('official hige dandism', 'pretender').get_similar()
-    return song
-@ks.route('/getsong', methods=["POST", 'GET'])
-def getsong():
+    if(request.method == 'POST'):
+        artist = request.form.get('artist')
+        song = request.form.get('song')
+        sort_method = request.form.get('sorttype')
+
+        return render_template('getsongs.html', song = Song(artist, song, 'similarity'), background="https://cdn.wallpapersafari.com/91/31/z4AvR6.jpg")
+
+    return render_template('getsongs.html', background="https://cdn.wallpapersafari.com/91/31/z4AvR6.jpg", song = Song('Queen', "Bohemian+Rhapsody", "similarity"))
+def bubbleSort():
     if(request.method == 'POST'):
         artist = request.form.get('artist')
         song = request.form.get('song')

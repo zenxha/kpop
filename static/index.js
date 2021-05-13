651188eb1e024749d7c6578631fe89ce262ae265
zenxha <komays5042@gmail.com>
2021-05-13 13:02:39 -0700

    js and css assets

---
+++ static/index.js
@@ -0,0 +1,29 @@
+function loop(img_count) {
+    let imgPath = "./static/";
+    setInterval(() => {
+        
+      let randomIndex = Math.floor(Math.random() * (img_count));
+      console.log('kek')
+      document
+        .getElementById("loop")
+        .setAttribute("src", `${imgPath}/${randomIndex}.png`);
+    }, 2000);
+  }
+function lol() {
+  let stuff = ['hi', 'hello', 'epic', 'kpop', 'jpop', 'poggers']
+  setInterval(() => {
+        
+    let r = stuff[Math.floor(Math.random() * stuff.length)]
+  
+    document
+      .getElementById("changingtext")
+      .innerHTML = r
+  }, 2000);
+}
+
+const xd = () => {
+  console.log('xd')
+}
+document.getElementsByTagName('body')[0].addEventListener("load", xd);
+
+  

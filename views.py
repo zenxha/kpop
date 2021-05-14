4342339beb4612c63a7d5a4cdb35cec5a0f4e241
Charlie <trollol678534@gmail.com>
2021-05-14 15:17:01 -0700

    reqs

--- views.py
+++ views.py
@@ -107,61 +107,6 @@ def easteregg():
     return render_template("easteregg/base.html", background="https://i.pinimg.com/originals/b8/e2/70/b8e270b7237f2f4c3a5905e6a3ca5f63.png")
 
 
-@app.route('/browse')
-def browse():
-    backgrounds = ["https://cdn.discordapp.com/attachments/784178874303905792/818606015494094868/812382.png"]
-    review_query = Review.query.all()
-    reviews = []
-
-    for review in review_query:
-        websiteurl = url_for('get_img', id=review.id)
-
-        review_dict = {
-            'id': review.id,
-            'username': review.username,
-            'content': review.content,
-            'satisfaction': review.satisfaction,
-            'image':  websiteurl
-        }
-        reviews.append(review_dict)
-    return render_template("homesite/browse.html", reviews=reviews, background=random.choice(backgrounds))
-
-
-@app.route('/easteregg/crossover')
-def crossover():
-    return render_template("easteregg/crossover.html")
-
-@app.route('/upload', methods=["POST", 'GET'])
-def upload():
-    background = random.choice(backgrounds)
-    if request.method == "POST":
-        name = request.form["username"]
-        satisfaction = request.form["satisfaction"]
-        content = request.form["content"]
-        image = request.files.get('img')
-        if not image:
-            return 'bad news ur image didnt make it to our servers :((((', 400
-
-        filename = secure_filename(image.filename)
-        mimetype = image.mimetype
-        if not filename or not mimetype:
-            return 'Bad upload', 400
-
-        review = Review(username=name, content=content, img=image.read(), filename=filename, satisfaction=satisfaction,mimetype=mimetype)
-        db.session.add(review)
-        db.session.commit()
-        return redirect(url_for("browse"))
-    return render_template("homesite/loginv2.html", background=background)
-
-@app.route('/images/<int:id>')
-def get_img(id):
-    img = Review.query.filter_by(id=id).first()
-    if not img:
-        return 'No img with that id', 200
-
-    return Response(img.img, mimetype=img.mimetype)
-
-
 "Login Section"
 
 

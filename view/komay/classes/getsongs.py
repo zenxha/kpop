720b6cd0737a8ac7ecf4058e69403446cdfc62b4
Charlie <trollol678534@gmail.com>
2021-06-08 13:13:15 -0700

    visual

--- view/komay/classes/getsongs.py
+++ view/komay/classes/getsongs.py
@@ -28,7 +28,7 @@ class Song:
 
         res_array = []
 
-        if res['similartracks']:
+        if 'similartracks' in res:
             for song in res['similartracks']['track']:
                 res_array.append(song)
 
@@ -42,5 +42,14 @@ class Song:
                 print(self._sorttype)
 
             return res_array
-        return []
-
+        else:
+            return [                {
+                "name": "Check your parameters again",
+                "playcount": 0,
+                "match": 0,
+                "url": "https://www.last.fm/music/Official+HIGE+DANdism/_/Shukumei",
+                "artist": {
+                    "name": "Invalid Artist Link",
+                    "url": "https://www.last.fm/music/Official+HIGE+DANdism"
+                },
+            }]
